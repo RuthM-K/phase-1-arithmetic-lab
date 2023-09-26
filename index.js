fad7fecbfbdc8f646fe4a351ae73e380e5ab4c47
Ruth <ruthmusyoka2000@gmail.com>
2023-09-26 13:46:47 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31 
+
+const multiply = num1 * num2
+
+const random = 6
+
+let num3 = 4
+let num4 = 1
+const mod = num3/num4
+
+const max = (15, 16, 17, 20)
+console.log(max.max)
